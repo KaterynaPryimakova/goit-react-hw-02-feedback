e4f7d472a04bd401646a724c2bc9ccfc79495f1b
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-10 23:12:57 +0200

    start work

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class FeedbackOptions extends Component {
+  render() {
+    return (
+      <>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </>
+    );
+  }
+}
