fdeab5ae919620102bdb8c302a0ec666380f42cd
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-12 14:04:27 +0200

    complite

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = {
@@ -16,13 +19,41 @@ export class App extends Component {
     }));
   };
 
+  countTotalFeedback = () => {
+    const { good, bad, neutral } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
   render() {
+    const TotalFeedback = this.countTotalFeedback();
     return (
       <div>
-        <FeedbackOptions
-          options={['Good', 'Neutral', 'Bad']}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
+        <Section title={'Please leave your feedback'}>
+          <FeedbackOptions
+            options={['Good', 'Neutral', 'Bad']}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title={'Statistics'}>
+          {TotalFeedback > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </div>
     );
   }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-
-    return (
-      <>
-        {options.map((option, index) => {
-          return (
-            <button
-              onClick={onLeaveFeedback}
-              type="button"
-              value={option}
-              key={index}
-            >
-              {option}
-            </button>
-          );
-        })}
-      </>
-    );
-  }
-}
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      {options.map((option, index) => {
+        return (
+          <button
+            onClick={onLeaveFeedback}
+            type="button"
+            value={option}
+            key={index}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </>
+  );
+};
